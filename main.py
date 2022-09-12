e6c1f8c3b83cc3d36c2a931b3f3471796150b04c
stjefim <stjefim2@gmail.com>
2022-09-13 00:03:11 +0300

    Discriminator and generator learn sequentially now.

--- main.py
+++ main.py
@@ -119,22 +119,27 @@ seed = tf.random.normal([num_examples_to_generate, noise_dim])
 def train_step(images):
     noise = tf.random.normal([BATCH_SIZE, noise_dim])
 
-    with tf.GradientTape() as gen_tape, tf.GradientTape() as disc_tape:
+    with tf.GradientTape() as disc_tape:
+      generated_images = generator(noise, training=True)
+
+      real_output = discriminator(images, training=True)
+      fake_output = discriminator(generated_images, training=True)
+      
+      disc_loss = discriminator_loss(real_output, fake_output)
+
+    gradients_of_discriminator = disc_tape.gradient(disc_loss, discriminator.trainable_variables)
+    discriminator_optimizer.apply_gradients(zip(gradients_of_discriminator, discriminator.trainable_variables))
+    
+    with tf.GradientTape() as gen_tape:
       generated_images = generator(noise, training=True)
 
       real_output = discriminator(images, training=True)
       fake_output = discriminator(generated_images, training=True)
 
       gen_loss = generator_loss(fake_output)
-      disc_loss = discriminator_loss(real_output, fake_output)
-
 
     gradients_of_generator = gen_tape.gradient(gen_loss, generator.trainable_variables)
-    gradients_of_discriminator = disc_tape.gradient(disc_loss, discriminator.trainable_variables)
-
     generator_optimizer.apply_gradients(zip(gradients_of_generator, generator.trainable_variables))
-    discriminator_optimizer.apply_gradients(zip(gradients_of_discriminator, discriminator.trainable_variables))
-    
 
 def train(dataset, epochs):
   for epoch in range(epochs):
@@ -142,7 +147,6 @@ def train(dataset, epochs):
 
     for image_batch in dataset:
       train_step(image_batch)
-      print("step done")
 
     generate_and_save_images(generator, epoch + 1, seed)
 
